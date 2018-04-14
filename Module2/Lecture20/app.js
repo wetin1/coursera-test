28d2ce29e9b5139c4edb6497fc1c521cae4ac645
Carlos Pérez <wete_penhafiel@hotmail.com>
2018-04-14 12:30:58 +0200

    Subir los ficheros de código de las lecciones del módulo 2

---
+++ Module2/Lecture20/app.js
@@ -0,0 +1,57 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListApp', [])
+.controller('ShoppingListAddController', ShoppingListAddController)
+.controller('ShoppingListShowController', ShoppingListShowController)
+.service('ShoppingListService', ShoppingListService);
+
+ShoppingListAddController.$inject = ['ShoppingListService'];
+function ShoppingListAddController(ShoppingListService) {
+  var itemAdder = this;
+
+  itemAdder.itemName = "";
+  itemAdder.itemQuantity = "";
+
+  itemAdder.addItem = function () {
+    ShoppingListService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+  }
+}
+
+
+ShoppingListShowController.$inject = ['ShoppingListService'];
+function ShoppingListShowController(ShopListService) {
+  var showList = this;
+
+  showList.items = ShopListService.getItems();
+
+  showList.removeItem = function (itemIndex) {
+    ShopListService.removeItem(itemIndex);
+  };
+}
+
+
+function ShoppingListService() {
+  var service = this;
+
+  // List of shopping items
+  var items = [];
+
+  service.addItem = function (itemName, quantity) {
+    var item = {
+      name: itemName,
+      quantity: quantity
+    };
+    items.push(item);
+  };
+
+  service.removeItem = function (itemIdex) {
+    items.splice(itemIdex, 1);
+  };
+
+  service.getItems = function () {
+    return items;
+  };
+}
+
+})();
